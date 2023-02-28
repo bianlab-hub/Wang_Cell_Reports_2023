041378fbee722c0ae81e7adc42b7b705eb2df950
bianlab-hub <82528955+bianlab-hub@users.noreply.github.com>
2023-02-28 16:51:24 +0800

    Update Fig2C_DEG_ratio_distance.sh

--- Fig2C_DEG_ratio_distance.sh
+++ Fig2C_DEG_ratio_distance.sh
@@ -1,8 +1,6 @@
 awk '{if($6==1) print($3 "\t" $4 "\t" $4+1 "\t" $1 "\t" $2);else print($3 "\t" $5 "\t" $5+1 "\t" $1 "\t" $2)}' Genes_log2fc.txt |\
 sort -k1,1 -k2,2n > Genes_tss_id_log2fc.txt  # 34556
-
 awk '{printf($1"\t");printf("%d\t", ($2+$3)/2);printf("%d\n", ($2+$3)/2+1)}' satb1_peaks.bed | sort -k1,1 -k2,2n > satb1_peaks_center.bed  # 24263
-
 bedtools closest -a Genes_tss_id_log2fc.txt -b satb1_peaks_center.bed > Genes_satb1.bed
 
 ## 0-10kb
@@ -13,7 +11,6 @@ sort 0_10kb_genes.bed down_genes.txt | uniq -d | wc -l  # 394
 # 388/13167=0.02946761
 # 394/13167=0.02992329
 # 12385/13167=0.9406091
-
 
 ## 10-20kb
 awk '{if(($2-$7>10000 && $2-$7<=20000) || ($2-$7<-10000 && $2-$7>=-20000)) print $4}' Genes_satb1.bed > 10_20kb_genes.bed  # 3711
@@ -34,7 +31,6 @@ sort 20_30kb_genes.bed down_genes.txt | uniq -d | wc -l  # 33
 # 33/2791=0.01182372
 # 2711/2791=0.9713364
 
-
 ## 30-40kb
 awk '{if(($2-$7>30000 && $2-$7<=40000) || ($2-$7<-30000 && $2-$7>=-40000)) print $4}' Genes_satb1.bed > 30_40kb_genes.bed  # 2159
 sort 30_40kb_genes.bed up_genes.txt | uniq -d | wc -l  # 47
@@ -43,7 +39,6 @@ sort 30_40kb_genes.bed down_genes.txt | uniq -d | wc -l  # 27
 # 47/2159=0.02176934
 # 27/2159=0.01250579
 # 2058/2159=0.9532191
-
 
 ## 40-50kb
 awk '{if(($2-$7>40000 && $2-$7<=50000) || ($2-$7<-40000 && $2-$7>=-50000)) print $4}' Genes_satb1.bed > 40_50kb_genes.bed  # 1780
@@ -54,7 +49,6 @@ sort 40_50kb_genes.bed down_genes.txt | uniq -d | wc -l  # 23
 # 23/1780=0.01292135
 # 1717/1780=0.9646067
 
-
 ## 50-60kb
 awk '{if(($2-$7>50000 && $2-$7<=60000) || ($2-$7<-50000 && $2-$7>=-60000)) print $4}' Genes_satb1.bed > 50_60kb_genes.bed  # 1370
 sort 50_60kb_genes.bed up_genes.txt | uniq -d | wc -l  # 23
@@ -63,7 +57,6 @@ sort 50_60kb_genes.bed down_genes.txt | uniq -d | wc -l  # 15
 # 23/1370=0.01678832
 # 15/1370=0.01094891
 # 1332/1370=0.9722628
-
 
 ## 60-70kb
 awk '{if(($2-$7>60000 && $2-$7<=70000) || ($2-$7<-60000 && $2-$7>=-70000)) print $4}' Genes_satb1.bed > 60_70kb_genes.bed  # 1054
@@ -74,7 +67,6 @@ sort 60_70kb_genes.bed down_genes.txt | uniq -d | wc -l  # 10
 # 10/1054=0.009487666
 # 1021/1054=0.9686907
 
-
 ## 70-80kb
 awk '{if(($2-$7>70000 && $2-$7<=80000) || ($2-$7<-70000 && $2-$7>=-80000)) print $4}' Genes_satb1.bed > 70_80kb_genes.bed  # 886
 sort 70_80kb_genes.bed up_genes.txt | uniq -d | wc -l  # 14
@@ -84,7 +76,6 @@ sort 70_80kb_genes.bed down_genes.txt | uniq -d | wc -l  # 13
 # 13/886=0.01467269
 # 859/886=0.969526
 
-
 ## 80-90kb
 awk '{if(($2-$7>80000 && $2-$7<=90000) || ($2-$7<-80000 && $2-$7>=-90000)) print $4}' Genes_satb1.bed > 80_90kb_genes.bed  # 736
 sort 80_90kb_genes.bed up_genes.txt | uniq -d | wc -l  # 21
@@ -93,7 +84,6 @@ sort 80_90kb_genes.bed down_genes.txt | uniq -d | wc -l  # 11
 # 21/736=0.02853261
 # 11/736=0.01494565
 # 704/736=0.9565217
-
 
 ## 90-100kb
 awk '{if(($2-$7>90000 && $2-$7<=100000) || ($2-$7<-90000 && $2-$7>=-100000)) print $4}' Genes_satb1.bed > 90_100kb_genes.bed  # 673
