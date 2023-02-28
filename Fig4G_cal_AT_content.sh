ecff3ab5f89f2c653f8320d86c7b6c2ba463aa7d
bianlab-hub <82528955+bianlab-hub@users.noreply.github.com>
2023-02-28 17:40:08 +0800

    Update Fig4G_cal_AT_content.sh

--- Fig4G_cal_AT_content.sh
+++ Fig4G_cal_AT_content.sh
@@ -12,3 +12,7 @@ bedtools getfasta -fi hg38.fa -bed satb1_only_250bp.bed | grep -v chr > satb1_on
 bedtools intersect -a ctcf_peaks.bed -b satb1_peaks.bed -wa -v | uniq |\
 awk '{printf($1 "\t");printf("%d\t", ($2+$3)*0.5-250);printf("%d\t", ($2+$3)*0.5+250+1);printf($4"\n")}' > ctcf_only_250bp.bed
 bedtools getfasta -fi hg38.fa -bed ctcf_only_250bp.bed | grep -v chr > ctcf_only_250bp.txt
+
+## Random control
+bedtools random -g hg38.chrom.sizes -l 501 -n 5000 -seed 0 | awk '{print $1 "\t" $2 "\t" $3}' > random.bed
+bedtools getfasta -fi hg38.fa -bed random.bed | grep -v chr > random_250bp.txt
