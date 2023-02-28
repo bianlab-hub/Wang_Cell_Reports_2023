d3de905e5993d9b462fff1a4126fdb3af7fe4c7b
bianlab-hub <82528955+bianlab-hub@users.noreply.github.com>
2023-02-28 18:23:57 +0800

    Update Fig6A_cal_loop_oe_ratio.R

--- Fig6A_cal_loop_oe_ratio.R
+++ Fig6A_cal_loop_oe_ratio.R
@@ -1,3 +1,4 @@
+## calculate oe ratio
 shctrl_1 <- read.table('shctrl_1_loops_oe_KR_10kb.txt', stringsAsFactors = FALSE)[, 7:13]
 colnames(shctrl_1) <- c('chr_1', 'left_1', 'left_2', 'chr_2', 'right_1', 'right_2', 'shctrl_1_oe')
 
@@ -17,3 +18,33 @@ dat <- merge(shctrl, shsatb1, by = c('chr_1', 'left_1', 'left_2', 'chr_2', 'righ
 dat$ratio <- (dat$shsatb1_1_oe + dat$shsatb1_2_oe) / (dat$shctrl_1_oe + dat$shctrl_2_oe)
 
 write.table(dat, "shctrl_shsatb1_oe.txt", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+
+## plot heatmap
+library(pheatmap)
+# up-regulated
+up <- read.table('up_loops.txt', stringsAsFactors = FALSE)[, 7:10]
+colnames(up) <- c('shctrl_1', 'shctrl_2', 'shsatb1_1', 'shsatb1_2')
+pdf('up_loops.pdf')
+pheatmap(up, cluster_row = FALSE, cluster_cols = FALSE,
+         scale = 'row', show_rownames = FALSE, breaks = seq(-1.5, 1.5, 0.03),
+         color = colorRampPalette(c('blue', 'white', 'red'))(100))
+dev.off()
+
+# down-regulated
+down <- read.table('down_loops.txt', stringsAsFactors = FALSE)[, 7:10]
+colnames(down) <- c('shctrl_1', 'shctrl_2', 'shsatb1_1', 'shsatb1_2')
+pdf('down_loops.pdf')
+pheatmap(down, cluster_row = FALSE, cluster_cols = FALSE,
+         scale = 'row', show_rownames = FALSE, breaks = seq(-1.5, 1.5, 0.03),
+         color = colorRampPalette(c('blue', 'white', 'red'))(100))
+dev.off()
+
+# unchanged
+unchanged <- read.table('unchanged_loops.txt', stringsAsFactors = FALSE)[, 7:10]
+colnames(unchanged) <- c('shctrl_1', 'shctrl_2', 'shsatb1_1', 'shsatb1_2')
+pdf('unchanged_loops.pdf')
+pheatmap(unchanged, cluster_row = FALSE, cluster_cols = FALSE,
+         scale = 'row', show_rownames = FALSE, breaks = seq(-1.5, 1.5, 0.03),
+         color = colorRampPalette(c('blue', 'white', 'red'))(100))
+dev.off()
