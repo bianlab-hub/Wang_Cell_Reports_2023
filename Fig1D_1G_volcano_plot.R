94478c78fa44057b215e9d19f21b779704c9e8d7
bianlab-hub <82528955+bianlab-hub@users.noreply.github.com>
2023-02-28 16:13:46 +0800

    Update Fig1D_1G_volcano_plot.R

--- Fig1D_1G_volcano_plot.R
+++ Fig1D_1G_volcano_plot.R
@@ -1,11 +1,12 @@
+library(ggplot2)
+library(ggthemes)
+
+## Jurkat T cell (shSATB1 vs. shCtrl)
 genes <- read.table("tss_log2fc_padj.txt", stringsAsFactors = FALSE)
 colnames(genes) <- c("chr", "start", "end", "log2fc", "padj")
 genes <- na.omit(genes)
 threshold <- as.factor(ifelse(genes$padj < 0.05 & abs(genes$log2fc) > 0.5,
                        ifelse(genes$log2fc > 0.5, 'Up', 'Down'), 'Not'))
-
-library(ggplot2)
-library(ggthemes)
 p <- ggplot(genes, aes(x = log2fc, y = -log10(padj), color = threshold)) +
             geom_point(size = 1, alpha = 0.5) +
             scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 30)) +  
@@ -14,3 +15,18 @@ p <- ggplot(genes, aes(x = log2fc, y = -log10(padj), color = threshold)) +
             geom_vline(xintercept = c(-0.5, 0.5), lty = 4, col = "black", lwd = 0.5) +
             geom_hline(yintercept = -log10(0.05), lty = 4, col = "black", lwd = 0.5)
 ggsave(p, file = "satb1_RNAseq_volcano_plot.pdf")
+
+
+## Mouse primary CD4+ naive T cell (Satb1 flox/flox CD4-Cre vs. Satb1+/+ CD4-Cre)
+genes <- read.table("mouse_cko_rnaseq.txt", stringsAsFactors = FALSE, header=TRUE)
+colnames(genes) <- c("gene_id", "log2fc", "padj")
+threshold <- as.factor(ifelse(genes$padj < 0.05 & abs(genes$log2fc) > 0.5,
+                       ifelse(genes$log2fc > 0.5, 'Up', 'Down'), 'Not'))
+p <- ggplot(genes, aes(x = log2fc, y = -log10(padj), color = threshold)) +
+            geom_point(size = 1, alpha = 0.5) +
+            scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 30)) +  
+            scale_x_continuous(limits = c(-10, 10), breaks = seq(-10, 10, 2)) + 
+            scale_color_manual(values = c("blue", "grey", "red")) + theme_few() +
+            geom_vline(xintercept = c(-0.5, 0.5), lty = 4, col = "black", lwd = 0.5) +
+            geom_hline(yintercept = -log10(0.05), lty = 4, col = "black", lwd = 0.5)
+ggsave(p, file = "mouse_cko_RNAseq_volcano_plot.pdf")
